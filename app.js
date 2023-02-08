026952e81832fce728fd1163d69d3b8b03d8694c
jimmyatv <rasic.alexandar@gmail.com>
2023-02-08 22:47:07 +0100

    Delete button completed, pagination left

--- app.js
+++ app.js
@@ -2,21 +2,9 @@ const addElement = document.querySelector('.add');
 const groceryElement = document.querySelector('.grocery');
 const groceryListElement = document.querySelector('.groceryListBody');
 
-
 const list = ['bread', 'milk', 'onion'];
 
-window.onload = () => {
-    list.forEach((element, idx) => {
-        groceryListElement.innerHTML += `
-        <tr>
-            <td>${element}</td>
-            <td><button class="btn delete-${idx}" ><i class="fa-solid fa-eraser"></i></button></td>
-        </tr>`
-    })
-}
-
-
-const read = () => {
+const read = (list) => {
     
     groceryListElement.innerHTML = '';
     
@@ -29,16 +17,26 @@ const read = () => {
     })
 }
 
-
 addElement.addEventListener('click', () => {
     if(groceryElement.value !== ''){
         list.push(groceryElement.value);
     }
 
-
     groceryElement.value = '';
     groceryElement.focus();
-    read();
-})
+    read(list);
+});
 
+const deleteItem = (event) => {
+    
+    let klasa = event.target.parentNode.className;
+    let idx = klasa.split(' ').slice(-1)[0].split('-')[1];
+    console.log(idx);
 
+    list.splice(idx, 1);
+    read(list);
+}
+
+window.onload = () => {
+    read(list);
+}
