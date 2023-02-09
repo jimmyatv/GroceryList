46a65067fddb7aeae3d2ccb37383b9ee5269b4cb
jimmyatv <rasic.alexandar@gmail.com>
2023-02-09 20:04:00 +0100

    Grocery list , ended

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 const addElement = document.querySelector('.add');
 const groceryElement = document.querySelector('.grocery');
 const groceryListElement = document.querySelector('.groceryListBody');
+const lessElement = document.querySelector('.less');
+const moreElement = document.querySelector('.more');
+const itemElement = document.querySelector('.item');
+
 
 const list = ['bread', 'milk', 'onion'];
+
+let page = 1; // current page
+let maxPage; // maximum pages
 
 const read = (list) => {
     
     groceryListElement.innerHTML = '';
     
-    list.forEach((element, idx) => {
+    loadFirstSix(list, (page - 1) * 6, 6).forEach((element, idx) => {
         groceryListElement.innerHTML += `
         <tr>
             <td>${element}</td>
             <td><button class="btn delete-${idx}" onClick="deleteItem(event)"><i class="fa-solid fa-eraser"></i></button></td>
         </tr>`
     })
+    list.length % 6 === 0 ? maxPage = list.length / 6 : maxPage = Math.floor(list.length / 6) + 1;
+    itemElement.value = page
 }
 
 addElement.addEventListener('click', () => {
@@ -38,5 +47,27 @@ const deleteItem = (event) => {
 }
 
 window.onload = () => {
+
+    list.length % 6 === 0 ? maxPage = list.length / 6 : maxPage = Math.floor(list.length / 6) + 1;
+    console.log(maxPage);
     read(list);
 }
+
+lessElement.addEventListener('click', () => {
+    page <= 1 ? page = 1 : page -= 1;
+    read(list);
+})
+
+moreElement.addEventListener('click', () => {
+    page >= maxPage ? page = maxPage : page += 1;
+    read(list);
+})
+
+const loadFirstSix = (lista, fromElementIdx, noOfElements) => {
+    return lista.filter((element, idx) => {
+        if(idx >= fromElementIdx && idx < fromElementIdx + noOfElements) {
+            return element;
+        };
+    });
+}
+
